635ea919324fb6adcdb2cac00327e1771a769920
GiordaniSantos <dani.santossilveira@hotmail.com>
2020-09-17 10:47:01 -0300

    Project created

---
+++ backend/src/main/java/com/curso/gmpesquisa/GmpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.curso.gmpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmpesquisaApplication.class, args);
+	}
+
+}
