9d3a8ce6181891f8a6a99bd08c3302b8c83a674d
GiordaniSantos <dani.santossilveira@hotmail.com>
2020-09-22 10:57:41 -0300

    DTO, Service

--- backend/src/main/java/com/curso/gmpesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/curso/gmpesquisa/repositories/RecordRepository.java
@@ -1,9 +1,11 @@
 package com.curso.gmpesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.curso.gmpesquisa.entities.Record;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 	
 
---
+++ backend/src/main/java/com/curso/gmpesquisa/services/GameService.java
@@ -0,0 +1,25 @@
+ package com.curso.gmpesquisa.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.curso.gmpesquisa.dto.GameDTO;
+import com.curso.gmpesquisa.entities.Game;
+import com.curso.gmpesquisa.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List<Game> list = repository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
